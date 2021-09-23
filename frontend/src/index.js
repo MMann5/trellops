ae14b834fefe07e2767dbfdac31db37a8c9d1a87
KotiMango <rons20000@gmail.com>
2021-09-23 12:45:17 +0300

    init Setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom';
 // import { Provider } from 'react-redux'
 import { App } from './RootCmps';
-import { store } from "./store/store.js";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import { store } from './store/store.js';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   // <Provider store={store}>
@@ -15,11 +15,10 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister()
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
