e9d118d0e9e5110c771e55cb74b58cc4cade8aeb
KotiMango <rons20000@gmail.com>
2021-09-23 19:02:37 +0300

    Fixed href endpoints

--- frontend/src/cmps/home-header.jsx
+++ frontend/src/cmps/home-header.jsx
@@ -1,17 +1,21 @@
-import logo from '../assets/imgs/home-logo.svg'
+import logo from '../assets/imgs/home-logo.svg';
 export function HomeHeader() {
-    return (
-        <section className="home-header">
-            <nav className="flex justify-space-between">
-                <div className="logo">
-                    <img src={logo} alt="" />
-                    <a href="">Trellops</a>
-                </div>
-                <div className="nav-btns">
-                    <a className="login-btn" href="">Log in</a>
-                    <a className="signup-btn" href="">Sign up</a>
-                </div>
-            </nav>
-        </section>
-    )
+  return (
+    <section className='home-header'>
+      <nav className='flex justify-space-between'>
+        <div className='logo'>
+          <img src={logo} alt='' />
+          <a href='/#'>Trellops</a>
+        </div>
+        <div className='nav-btns'>
+          <a className='login-btn' href='/#'>
+            Log in
+          </a>
+          <a className='signup-btn' href='/#'>
+            Sign up
+          </a>
+        </div>
+      </nav>
+    </section>
+  );
 }
